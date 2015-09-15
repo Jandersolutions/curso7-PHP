9d4eb4f18e8efecf4c88b58a554b72b4ee0bbffc
Janderson Batista Abreu <jba1719@gmail.com>
2015-09-14 23:47:02 -0300

    adição de arquivo array2.php

--- public/array.php
+++ public/array.php
@@ -2,7 +2,12 @@
   include('header.php');
 
   require_once ('functions.php');
-
-
+  echo "<h1>".$contas['Agosto']."</h1>";
+  echo "</br>";
+  echo "<h2>".$contas_array[4][1]."</h2>";
+  echo "</br>";
+  echo "<h3>".$contas_array[0]."</h3>";
+  echo "</br>";
+  echo $contas_novas[3];
   include('footer.php');
 ?>
