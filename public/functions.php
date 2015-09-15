636be8c8648a84f0b1731ce500133b50f7d6af6e
Janderson Batista Abreu <jba1719@gmail.com>
2015-09-14 23:47:02 -0300

    adição de arquivo array2.php

--- public/functions.php
+++ public/functions.php
@@ -3,8 +3,32 @@
   $nome .= ' Batista ';
   $nome .= ' Abreu';
   $flutuantes = '3.568932';
+  $contas = array(
+    'Janeiro' => 'Conta de agua',
+    'Fevereiro' => 'Conta de luz',
+    'Março' => 'Conta de telefone',
+    'Agosto' => "$nome"
+);
+$contas_array = array(
+  'Conta de agua',
+  'Conta de luz',
+  'Conta de telefone',
+  'Janderson',
+  array(
+    'Conta de gaz',
+    'Conta de luz'
+  )
+);
+$contas_novas = [
+  'Conta de agua',
+  'Conta de luz',
+  'Conta de telefone',
+  'Janderson'
+];
 
-  echo "Meu nome e $nome";
-  echo "</br>";
-  echo 'Meu nome e '. $nome ;
+
+
+  //echo "Meu nome e $nome";
+  //echo "</br>";
+  //echo 'Meu nome e '. $nome ;
 ?>
