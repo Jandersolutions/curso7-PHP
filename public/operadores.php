5139cdcb5f4b5ef0a5e1358dbde426b761c3fe89
Janderson Batista Abreu <jba1719@gmail.com>
2015-09-17 08:07:35 -0300

    adição de operadores.php

--- public/operadores.php
+++ public/operadores.php
@@ -1,7 +1,43 @@
 <?php
 include('header.php');
 
-require_onde('functions.php');
+$x = 10;
+$y = 10;
 
+//comparar
+if($x == $y){
+  echo "X e igual a Y";
+}
+//identico
+$y = '10';
+if($x === $y){
+  echo "X e identico a y";
+}
+echo "</br>";
+//operador maior
+if($x > $y){
+  echo " X e maior que Y";
+}
+echo "</br>";
+//menor
+if($x < $y){
+  echo "X e menor que Y";
+}
+$x = 150;
+$y = 100;
+echo "</br>";
+if($x <= $y){
+  echo "X e menor igual a Y";
+}
+$y = 50;
+$x = 100;
+echo "</br>";
+if($x >= $y){
+  echo "X e maior igual a Y";
+}
+echo "</br>";
+if($x <> $y){
+  echo "X e diferente de Y";
+}
 include('footer.php');
  ?>
