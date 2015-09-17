4ce96512e08e3fd9d8d879a8d004e07228f83952
Janderson Batista Abreu <jba1719@gmail.com>
2015-09-17 08:46:45 -0300

    adição de for.php

---
+++ public/while.php
@@ -0,0 +1,12 @@
+<?php
+include('header.php');
+
+//require_once('functions.php');
+$i = 0;
+while ($i <= 100) {
+  echo "<h2>".$i . " "."</h2>";
+  $i++; //incrementa
+}
+
+include('footer.php');
+ ?>
