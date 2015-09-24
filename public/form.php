c180539112974dff72664b567f7d7602f1de910c
Janderson Batista Abreu <jba1719@gmail.com>
2015-09-24 00:27:38 -0300

    Adição de função alert em form.php

--- public/form.php
+++ public/form.php
@@ -1,11 +1,27 @@
 <?php
+if(isset($_POST['submit'])){
+    $username = $_POST['user'];
+    $password = $_POST['password'];
+    $message = "Bem vindo: " . $username;
+    if($password == ""){
+        $message = "Senha invalida";
+    }
+    if($username == ""){
+        $message = "Nome invalido";
+    }
+}else{
+$message = "Por favor efetue login";
+}
+?>
+<?php
 include('header.php');
 
 require_once('var.php');
 ?>
+<?php echo "<h2>". $message."</h2>"; ?>
 <content class="row">
     <div class="col-lg-4 col-sd-4 col-lg-push-4 well formulario">
-<form class="form-group" action="action.php" method="post">
+<form class="form-group" action="form.php" method="post">
     <h4><i class="fa fa-user"></i>Usuario:</h4> <input type="text" class="form-control" name="user" value=""/>
     <br />
     <h4><i class="fa fa-key"></i>Senha:</h4> <input type="password" class="form-control" name="password" value=""/>
